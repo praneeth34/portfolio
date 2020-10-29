ac4268f93a3ca0b45d87afbb5625f90dc7984e08
NightSlayer <praneethkumarpk34@gmail.com>
2020-10-30 00:16:42 +0530

    added carousel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 //components
 import MyNavbar from './components/my-navbar/mynavbar.component'
+import MyCarousal from './components/my-carousal/my-carousal.component'
+import TitleMessage from './components/title-message/title-message.component'
+
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
      <MyNavbar/>
-       Portfolio
+     <MyCarousal/>
+    <TitleMessage/>
     </div>
   );
 }
